38be2fcd392124d5c4d4e5863addae5fe6539eb6
Shreyank Gowda <shreyankg41@gmail.com>
2026-03-03 19:15:54 +0530

    added token schema

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -1,1 +1,11 @@
 #Add the pydantic models for the schemas here
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: int | None = None
